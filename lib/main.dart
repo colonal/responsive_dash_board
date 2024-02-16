85e3b32395058306963805b4dfd1d7314ec4c468
LAPTOP-KFFSGVHG\user <colonal.ch@gmail.com>
2024-02-16 20:09:14 +0300

    Responsive Dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'utils/custom_scroll_behavior.dart';
 import 'views/dash_board_view.dart';
 
 void main() {
@@ -14,9 +15,10 @@ class ResponsiveDashBoard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DashBoardView(),
+      scrollBehavior: MyCustomScrollBehavior(),
+      home: const DashBoardView(),
     );
   }
 }
